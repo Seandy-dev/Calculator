30169678f7071b2901fac6619afc3c53d7946569
Sindiswa Mbhele <219279616@mycput.com>
2021-03-15 13:21:07 +0200

    First commit

--- src/za/ac/cput/calculator/Division.java
+++ src/za/ac/cput/calculator/Division.java
@@ -1,15 +1,16 @@
 package za.ac.cput.calculator;
 
-/**
- * Return result of integer
- * numerator
- * denominator
- */
+
 
 /**
  * author Sindy
  */
 public class Division {
+    /**
+     * Return result of integer
+     * numerator
+     * denominator
+     */
     public static int integerDivision(int numerator, int denominator){
         if (denominator < 1) return 0;
         return numerator/denominator;
